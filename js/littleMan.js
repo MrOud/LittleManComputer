095c5e46626db96cc2438605b90e40dd2f84f4b1
Kris Oud <oudster@gmail.com>
2023-12-23 21:53:31 -0500

    first commit
    *basic layout
    *in stack entry
    *memory inserts

---
+++ js/littleMan.js
@@ -0,0 +1,153 @@
+//Defered
+
+/**
+ * MEMORY SECTION
+ */
+const memCells = document.getElementById('memCells')
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    //create our new cell
+    let newCell = document.createElement('div')
+    let cellAddy = document.createElement('p')
+    let cellValue = document.createElement('p')
+
+    //style & set
+    cellValue.id = "cell" + j + i
+    newCell.classList.add(
+      "bg-white",
+      "border",
+      "border-black",
+      "justify-stretch",
+      "rounded-md",
+      "m-1"
+    )
+
+    cellAddy.innerHTML = "" + j + i
+    cellAddy.classList.add(
+      "text-sm",
+      "text-white",
+      "text-right",
+      "bg-black",
+      "px-1"
+    )
+
+    cellValue.innerHTML = "---"
+
+    //Add to document
+    newCell.appendChild(cellAddy)
+    newCell.appendChild(cellValue)
+    memCells.appendChild(newCell)
+  }
+}
+
+/**
+ * INPUT SECTION
+ */
+const valueIn = document.getElementById("pushValue")
+const inSheet = document.getElementById("inSheet")
+let inArray = new Array()
+
+//Add a value to the 
+function pushIn() {
+  const inValue = Number(valueIn.value)
+  if (inValue >= 0 && inValue <= 1024 && inArray.length <= 20) {
+    inArray.push(Number(inValue))
+    updateInSheet()
+  }
+}
+
+function clearIn() {
+  inArray = new Array()
+  updateInSheet()
+}
+
+function validateIn() {
+  if (valueIn.value > 1024) valueIn.value = 1024
+  if (valueIn.value < 0) valueIn.value = 0
+}
+
+function updateInSheet() {
+  inSheet.innerHTML = ""
+  inArray.map((val) => {
+    console.log(val)
+    const p = document.createElement("p")
+    p.innerHTML = val
+    inSheet.prepend(p)
+  })
+}
+
+/**
+ * PROGRAM SECTION
+ */
+const progAddy = document.getElementById("progAddy")
+const progData = document.getElementById("progData")
+const progAutoInc = document.getElementById("progAutoInc")
+
+function validateProgData(data) {
+  if (isNaN(data)) {
+    console.log("Not valid integer")
+    return false
+  }
+
+  if (data < 0 || data > 999) {
+    console.log("Value out of range")
+    return false
+  }
+
+  return true
+}
+
+function validateProgAddy(addy) {
+  if (isNaN(addy)) {
+    console.log("Not valid integer")
+    return false
+  }
+
+  if (addy < 0 || addy > 99) {
+    console.log("Value out of range")
+    return false
+  }
+
+  return true
+}
+
+function validateProgInputs(addy, data) {
+  return validateProgAddy(addy) && validateProgData(data)
+}
+
+function pushProg() {
+  let addy = Math.floor(Number(progAddy.value))
+  const data = Math.floor(Number(progData.value))
+
+  if (!validateProgInputs(addy, data)) return
+
+  let addyHigh = Math.floor(addy / 10)
+  let addyLow = addy % 10
+
+  const dataHun = Math.floor(data / 100)
+  const dataTen = Math.floor((data / 10) % 10)
+  const dataOne = data % 10
+
+  const cell = document.getElementById("cell" + addyHigh + addyLow)
+  cell.innerHTML = `${dataHun}${dataTen}${dataOne}`
+
+  if (progAutoInc.checked) {
+    addy += 1
+    addyHigh = Math.floor(addy / 10)
+    addyLow = addy % 10
+
+    progAddy.value = `${addyHigh}${addyLow}`
+  }
+}
+
+function clearProgAddress() {
+  const addy = Math.floor(Number(progAddy.value))
+
+  if (!validateProgAddy(addy)) return
+
+  const addyHigh = Math.floor(addy / 10)
+  const addyLow = addy % 10
+
+  const cell = document.getElementById("cell" + addyHigh + addyLow)
+  cell.innerHTML = "---"
+}
